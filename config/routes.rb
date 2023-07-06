a6af5ed7f161c312324cf3ece078525008be0c38
Aditya Kumar Vishwakarma <aditya.vishwakarma@intrcity.com>
2023-07-06 17:33:31 +0530

    defined enpoints for the application

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "movies#index"
+  resources :movies, only: [:index, :show] do
+    get 'search', on: :collection
+  end
 end
